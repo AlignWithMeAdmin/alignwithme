006e3bfa2be463a9bc8253fa6d1d574e8287712a
Inesh-P <ineshp2020@gmail.com>
2024-08-02 17:41:40 +0530

    Add age group repository and controller to masterdata

---
+++ master_data_service/src/main/java/org/alignwithme/masterdata/AgeGroupRepository.java
@@ -0,0 +1,8 @@
+package org.alignwithme.masterdata;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface AgeGroupRepository extends MongoRepository<AgeGroup, String>{
+
+    
+}
